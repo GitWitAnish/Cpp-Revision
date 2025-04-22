fb12274e9e7db60413beb7d5f9a8751427fdf15e
Anish Karki <anishkarki5710@gmail.com>
2025-04-22 13:59:59 +0545

    .....

---
+++ func.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+ using namespace std;
+ 
+ 
+ void myFunction() {
+     cout << "I just got executed!\n";
+   }
+   
+   int main() {
+     myFunction();
+     myFunction();
+     myFunction();
+     return 0;
+   }
+   
+   // I just got executed!
+   // I just got executed!
+   // I just got executed!
