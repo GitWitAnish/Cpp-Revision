11eb1fe482fe991cc8bfaf2f009909f085da8de5
Anish Karki <anishkarki5710@gmail.com>
2025-04-26 20:39:35 +0545

    enum

---
+++ enum.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+enum lnguages {C, Cpp, Java, Python, JavaScript};
+
+
+int main() {
+    
+     cout<< C <<endl;
+     cout<< Java <<endl;
+     cout<< JavaScript <<endl;
+    
+
+return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
